e8b7e886a6eed67cdc8388483146f2c6098007c1
Manavendra Sen <manavendra4288@gmail.com>
2022-09-13 01:04:36 +0530

    feat: add container layout

---
+++ components/Layout/Container.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface ContainerProps {
+  left: React.ReactNode;
+  children: React.ReactNode;
+  right: React.ReactNode;
+}
+
+export const Container: React.FC<ContainerProps> = ({
+  left,
+  children,
+  right,
+}) => {
+  return (
+    <div className='flex gap-6'>
+      <div className='w-1/4'>{left}</div>
+      <div className='grow w-1/2'>{children}</div>
+      <div className='w-1/4'>{right}</div>
+    </div>
+  );
+};
+
+export default Container;
