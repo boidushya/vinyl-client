e02b0619eeff9a70ba77443caadf0c85e5d38b58
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2022-09-19 04:27:42 +0530

    fix: chat bubble styles

--- components/ChatPanel/ChatBubble.tsx
+++ components/ChatPanel/ChatBubble.tsx
@@ -11,9 +11,14 @@ export const ChatBubble: React.FC<ChatBubbleProps> = ({
 	comment,
 }) => {
 	return (
-		<div className="border-t-[#E5D4FF20] border-t-2 px-9 py-5">
-			<p className="text-[#E5D4FF]">{username} guessed</p>
-			<p className="text-base">{comment}</p>
+		<div className="px-6 py-4 border-t-2 border-opacity-25 border-t-gray-600">
+			<p className="mb-1 text-sm font-bold text-opacity-75 text-violet-200">
+				<span className="text-opacity-100 text-violet-200">
+					{username}
+				</span>{" "}
+				guessed
+			</p>
+			<p className="text-base leading-snug text-gray-50">{comment}</p>
 		</div>
 	);
 };
--- components/ChatPanel/ChatInput.tsx
+++ components/ChatPanel/ChatInput.tsx
@@ -7,9 +7,9 @@ const ChatInput: React.FC<ChatInputProps> = () => {
 	return (
 		<div>
 			<div className="flex">
-				<input className="bg-[#404065] text-[#E5D4FF] outline-none w-full p-4 px-9 rounded-l-lg" />
+				<input className="bg-[#404065] text-[#E5D4FF] outline-none w-full py-3 px-6 rounded-bl-lg" />
 				<Button
-					className="rounded-r-lg"
+					className="rounded-none rounded-br-lg"
 					onClick={() => {
 						console.log("cc");
 					}}
