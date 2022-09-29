9259cb18eb9ff4d33b25cc2248d7b755098aea9e
Manavendra Sen <manavendra4288@gmail.com>
2022-09-29 13:46:13 +0530

    feat: canvas styling

--- components/MusicCanvasPanel/MusicCanvasPanel.tsx
+++ components/MusicCanvasPanel/MusicCanvasPanel.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import ReactHowler from "react-howler";
+import { ColorExtractor } from "react-color-extractor";
 import { MdVolumeUp, MdVolumeOff, MdPlayArrow } from "react-icons/md";
 
 interface MusicCanvasPanelProps {}
@@ -8,44 +9,66 @@ interface MusicCanvasPanelProps {}
 const MusicCanvasPanel: React.FC<MusicCanvasPanelProps> = () => {
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [isMuted, setIsMuted] = useState(false);
+	const [colors, setColors] = useState<String[]>([]);
 
 	let icon;
 	if (!isPlaying) {
-		icon = <MdPlayArrow />;
+		icon = <MdPlayArrow size={24} />;
 	} else {
-		if (isMuted) icon = <MdVolumeOff />;
-		else icon = <MdVolumeUp />;
+		if (isMuted) icon = <MdVolumeOff size={24} />;
+		else icon = <MdVolumeUp size={24} />;
 	}
+
+	const getColors = (imageColors: String[]) => {
+		setColors(imageColors);
+		console.log(imageColors);
+	};
 
 	const ALBUM_ART =
 		"https://i.scdn.co/image/ab67616d0000b273c79b600289a80aaef74d155d";
 	const MUSIC_URL =
 		"https://listen.rezo.live/preview/886/e3a103cb56c3fda02b23d528f3eacde1_96_p.mp4";
 
-	const NAME = "bla__ _____";
+	const NAME = "_____ _____";
 
 	return (
-		<div className="bg-[#27273E] rounded-xl flex items-center justify-center flex-col gap-9 py-9 h-full overflow-hidden">
-			<ReactHowler src={MUSIC_URL} playing={isPlaying} mute={isMuted} />
-			<img
-				src={ALBUM_ART}
-				alt=""
-				width={100}
-				height={100}
-				className="blur-xl w-72 h-72"
-			/>
-			<p className="font-bold text-2xl mt-8">{NAME}</p>
-
-			<button
-				className="p-4 rounded-full bg-[#4d4d79]"
-				onClick={() => {
-					if (!isPlaying) setIsPlaying(true);
-					else setIsMuted(!isMuted);
+		<>
+			<ColorExtractor src={ALBUM_ART} getColors={getColors} />
+			<div
+				className={`rounded-xl flex items-center justify-center flex-col gap-9 py-9 h-full overflow-hidden`}
+				style={{
+					// backgroundColor: `${colors[0]}`,
+					background: `linear-gradient(${colors[0]}, ${colors[4]})`,
 				}}
 			>
-				{icon}
-			</button>
-		</div>
+				<ReactHowler
+					src={MUSIC_URL}
+					playing={isPlaying}
+					mute={isMuted}
+				/>
+
+				<img
+					src={ALBUM_ART}
+					alt=""
+					width={100}
+					height={100}
+					className="blur-xl w-80 h-80 rounded-lg"
+				/>
+
+				<p className="font-bold text-3xl my-1 text-center tracking-wider">
+					{NAME}
+				</p>
+				<button
+					className={`p-4 rounded-full bg-[#ffffff50]`}
+					onClick={() => {
+						if (!isPlaying) setIsPlaying(true);
+						else setIsMuted(!isMuted);
+					}}
+				>
+					{icon}
+				</button>
+			</div>
+		</>
 	);
 };
 
