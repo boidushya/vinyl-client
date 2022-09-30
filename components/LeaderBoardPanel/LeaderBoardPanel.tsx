4593461f37abe3fa10daf4432bc88b0a4c618e25
somil24 <somil2402@gmail.com>
2022-10-01 02:40:55 +0530

    feat: leader board update

--- components/LeaderBoardPanel/LeaderBoardPanel.tsx
+++ components/LeaderBoardPanel/LeaderBoardPanel.tsx
@@ -7,52 +7,66 @@ interface LeaderBoardPanelProps {}
 
 
 
-const users = [
-	{
-		id: "dfads",
-		username: "mavn",
-		score: 242,
-	},
-	{
-		id: "fs",
-		username: "mavn",
-		score: 242,
-	},
-	{
-		id: "dffdaads",
-		username: "mavn",
-		score: 242,
-	},
-	{
-		id: "fas",
-		username: "mavn",
-		score: 242,
-	},
-];
+// const users = [
+// 	{
+// 		id: "dfads",
+// 		username: "mavn",
+// 		score: 242,
+// 	},
+// 	{
+// 		id: "fs",
+// 		username: "mavn",
+// 		score: 242,
+// 	},
+// 	{
+// 		id: "dffdaads",
+// 		username: "mavn",
+// 		score: 242,
+// 	},
+// 	{
+// 		id: "fas",
+// 		username: "mavn",
+// 		score: 242,
+// 	},
+// ];
 
 const LeaderBoardPanel: React.FC<LeaderBoardPanelProps> = ({}) => {
 
 	const {roomId} =useGameStore();
-   // const [users,setUsers]=useState([]);
+  	const [users, setUsers]=useState<any>();
 
+	
 	socket.on('updated-score-board',(result)=>{
-		console.log(result,"updated score board")
-	})
+		 const scores:any = Object.values(result)[0];
+		
+		const data = scores.map((score: any) => {
+			return ({ 
+				username: score[0],
+				score: score[1]
+			})
+		})
+
+		setUsers(data);
+		console.log(data);
+		
+	});
+		// console.log(result,"updated score board")
+	
 
 	return (
 		<div className="flex flex-col h-full gap-5 py-9">
 			<h2 className="text-xl font-semibold">Leaderboard</h2>
 			<div className="flex flex-col gap-4">
-				{users.map(user => (
+				{users && users.map((user: { username: string; score: number; }) => (
 					<LeaderBoardProfile
-						key={user.id}
-						id={user.id}
+						key={user.username}
+						id={user.username}
 						username={user.username}
 						score={user.score}
 						isWinning
 					/>
 				))}
-			</div>
+			</div> 
 		</div>
 	);
 };
