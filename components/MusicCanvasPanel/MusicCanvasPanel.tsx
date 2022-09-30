e1eec1a1b760db9eb6c18ac62ed7895588843856
Manavendra Sen <manavendra4288@gmail.com>
2022-09-30 23:24:20 +0530

    feat: add question id state

--- components/MusicCanvasPanel/MusicCanvasPanel.tsx
+++ components/MusicCanvasPanel/MusicCanvasPanel.tsx
@@ -4,13 +4,14 @@ import ReactHowler from "react-howler";
 import { ColorExtractor } from "react-color-extractor";
 import { MdVolumeUp, MdVolumeOff, MdPlayArrow } from "react-icons/md";
 import { socket } from "utils/webSocket";
+import useGameStore from "store/gameStore";
 
 interface MusicCanvasPanelProps {}
-interface tracks{
-	name:string
-	url:string
-	image:string
-	question_id:string
+interface tracks {
+	name: string;
+	url: string;
+	image: string;
+	question_id: string;
 }
 
 const MusicCanvasPanel: React.FC<MusicCanvasPanelProps> = () => {
@@ -21,8 +22,10 @@ const MusicCanvasPanel: React.FC<MusicCanvasPanelProps> = () => {
 	const [colors, setColors] = useState<String[]>([]);
 	const [tracks, setTracks] = useState<tracks>();
 	const playButtonRef = useRef(null);
-	const [albumURL,setAlbumURL]=useState(null);
-	const[albumImageUrl,setAlbumImageUrl]=useState('')
+	const [albumURL, setAlbumURL] = useState(null);
+	const [albumImageUrl, setAlbumImageUrl] = useState("");
+
+	const { setQuestionId } = useGameStore();
 
 	// useEffect(() => {
 	// 	if (playButtonRef !== null && playButtonRef.current)
@@ -48,19 +51,20 @@ const MusicCanvasPanel: React.FC<MusicCanvasPanelProps> = () => {
 
 	socket.on("tracksData", (data: any) => {
 		setTracks(data);
-		setAlbumURL(data.url)
+		setAlbumURL(data.url);
 		setAlbumImageUrl(data.image);
-		console.log(data,"tracks data ");
-	})
+		setQuestionId(data.question_id);
+		console.log(data, "tracks data ");
+	});
 
 	socket.on("counter", (data: any) => {
 		setCounter(data);
 		console.log(data, "Counter");
 	});
 
-	socket.on("game-end",()=>{
-		console.log('end game');
-	})
+	socket.on("game-end", () => {
+		console.log("end game");
+	});
 
 	// const ALBUM_ART =
 	// 	tracks?.image
@@ -88,45 +92,46 @@ const MusicCanvasPanel: React.FC<MusicCanvasPanelProps> = () => {
 	}, [counter]);
 
 	return (
-		albumURL &&
-		<>
-			<ColorExtractor src={albumImageUrl} getColors={getColors} />
-			<div
-				className={`rounded-xl flex items-center justify-center flex-col gap-9 py-9 h-full overflow-hidden`}
-				style={{
-					background: `linear-gradient(${colors[0]}, ${colors[2]})`,
-				}}
-			>
-				<ReactHowler
-					src={albumURL}
-					playing={isPlaying}
-					mute={isMuted}
-				/>
-
-				<img
-					src={albumImageUrl}
-					alt=""
-					width={100}
-					height={100}
-					className={`${blur} w-80 h-80 rounded-lg`}
-				/>
-
-				<p className="font-bold text-3xl my-1 text-center tracking-wider">
-					{counter}
-				</p>
-				<button
-					ref={playButtonRef}
-					// onFocus={playSong}
-					className={`p-4 rounded-full bg-[#ffffff50]`}
-					onClick={() => {
-						playSong();
+		albumURL && (
+			<>
+				<ColorExtractor src={albumImageUrl} getColors={getColors} />
+				<div
+					className={`rounded-xl flex items-center justify-center flex-col gap-9 py-9 h-full overflow-hidden`}
+					style={{
+						background: `linear-gradient(${colors[0]}, ${colors[2]})`,
 					}}
 				>
-					{icon}
-				</button>
-			</div>
-		</>
+					<ReactHowler
+						src={albumURL}
+						playing={isPlaying}
+						mute={isMuted}
+					/>
+
+					<img
+						src={albumImageUrl}
+						alt=""
+						width={100}
+						height={100}
+						className={`${blur} w-80 h-80 rounded-lg`}
+					/>
+
+					<p className="font-bold text-3xl my-1 text-center tracking-wider">
+						{counter}
+					</p>
+					<button
+						ref={playButtonRef}
+						// onFocus={playSong}
+						className={`p-4 rounded-full bg-[#ffffff50]`}
+						onClick={() => {
+							playSong();
+						}}
+					>
+						{icon}
+					</button>
+				</div>
+			</>
+		)
 	);
-}
+};
 
 export default MusicCanvasPanel;
